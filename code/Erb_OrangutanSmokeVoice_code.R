8c145f665197a6d83c4aec060c74aed94183082a
wendyerb <erbivorous@gmail.com>
2022-12-23 13:15:16 +0800

    code & data updates

--- code/Erb_OrangutanSmokeVoice_code.R
+++ code/Erb_OrangutanSmokeVoice_code.R
@@ -1,3 +1,6 @@
+## Install MuMIn package
+install.packages("MuMIn", repos="http://R-Forge.R-project.org")
+
 ## Load libraries
 library(tidyverse)
 library(lme4)
@@ -5,11 +8,13 @@ library(DHARMa)
 library(MuMIn)
 library(lmerTest)
 
-
 ## Set working directory
 setwd('/Users/Wendy/github/wildfire-smoke-orangutan-voice/data')
 
 ## Read files
+enivronment <- read.csv('environment.csv')
+context <- read.csv('context.csv')
+
 Ncall_PreSmo <- read.csv('N.LC_PreSmoke.csv')
 Ncall_PrePos <- read.csv('N.LC_PrePost.csv')
 
@@ -22,17 +27,57 @@ voice_PrePos<- read.csv('voice_PrePost.csv')
 nlp_PreSmo<- read.csv('nlp_PreSmoke.csv')
 nlp_PrePos <- read.csv('nlp_PrePost.csv')
 
-## Code for general linear mixed models & model residuals (use this for all variables in 'duration' and 'voice' data files; for jitter only, use "log(jit)" to log-transform this variable)
+
+# 1. Variation in environmental variables & call context across pe --------
+
+# Create & re-level factors to compare to Pre-Smoke reference
+enivronment$Period <- as.factor(enivronment$Period)
+enivronment$Period <- relevel(enivronment$Period, ref = 2)  
+
+context$Period <- as.factor(context$Period)
+context$Period <- relevel(context$Period, ref = 2)  
+
+
+## Temperature: all comparisons sig.
+period.temp = lm(MaxTemp ~ Period, data = enivronment)
+period.temp
+summary(period.temp)
+
+## Day rain: pre-smoke vs. smoke comparison sig.
+period.rain = lm(DayRain ~ Period, data = enivronment)
+period.rain
+summary(period.rain)
+
+## Context: no comparisons sig.
+period.spont = lm(Spontaneous ~ Period, data = context)
+period.spont
+summary(period.spont)
+
+
+# 2.  Model building for acoustic variables -------------------------------
+
+## General linear mixed models & model residuals (use this for all variables in 'voice' data file; for jitter only, use "log(jit)" to log-transform this variable)
 mod.f0.PreSmo = lmer(f0 ~ Period + (1 | Male.ID), data = voice_PreSmo)
 summary(mod.f0.PreSmo)
 plot(mod.f0.PreSmo)  
 require(DHARMa)
-fittedModel=mod.shim.PreSmo
+fittedModel=mod.f0.PreSmo
 simulationOutput=simulateResiduals(fittedModel=fittedModel)
 plot(simulationOutput)
 MuMIn::r.squaredGLMM(lmer(f0 ~ Period + (1 | Male.ID), data = voice_PreSmo))
 
-## Code for generalized linear mixed models (Poisson, log) & model residuals (use this for 'Ncall' (# long calls) data files)
+
+## General linear mixed model & model residuals including call context (use for 'duration')
+mod.durat.PreSmo = lmer(duration ~ Period + Spontaneous + (1 | Male.ID), data = duration_PreSmo)
+summary(mod.durat.PreSmo)
+plot(mod.durat.PreSmo)  
+require(DHARMa)
+fittedModel=mod.durat.PreSmo
+simulationOutput=simulateResiduals(fittedModel=fittedModel)
+plot(simulationOutput)
+MuMIn::r.squaredGLMM(lmer(duration ~ Period + Spontaneous + (1 | Male.ID), data = duration_PreSmo))
+
+## Generalized linear mixed models (Poisson, log) & model residuals (use this for 'Ncall' (# long calls) data files)
 mod.NLC.PreSmo = glmer(N.LC ~ Period + (1 | Male.ID), family = poisson(link = "log"), data = Ncall_PreSmo)
 summary(mod.NLC.PreSmo)
 plot(mod.NLC.PreSmo)  
@@ -42,7 +87,7 @@ simulationOutput=simulateResiduals(fittedModel=fittedModel)
 plot(simulationOutput)
 MuMIn::r.squaredGLMM(glmer(N.LC ~ Period + (1 | Male.ID), family = poisson(link = "log"), data = Ncall_PreSmo))
 
-## Code for generalized linear mixed models (binomial, logit) & model residuals (use this for variables in 'nlp' data files)
+## Generalized linear mixed models (binomial, logit) & model residuals (use this for variables in 'nlp' data files)
 mod.bp.PreSmo = glmer(BP.01 ~ Period + (1 | Male.ID), family = binomial(link = "logit"),  data = nlp_PreSmo)
 summary(mod.bp.PreSmo)
 plot(mod.bp.PreSmo)  
@@ -52,7 +97,7 @@ simulationOutput=simulateResiduals(fittedModel=fittedModel)
 plot(simulationOutput)
 MuMIn::r.squaredGLMM(glmer(BP.01 ~ Period + (1 | Male.ID), family = binomial(link = "logit"),  data = nlp_PreSmo))
 
-## Code for jittered boxplots
+## Jittered boxplots
 nlc.smoke <- ggplot(PreSmo_Ncall, aes(x = Period, y = N.LC)) +
   geom_boxplot(varwidth = FALSE, outlier.color = NA) +
   scale_x_discrete(labels = c("Pre-Smoke","Smoke")) +
